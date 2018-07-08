46c33a6baed344ad657d77e6373e4558c1d64a13
Mario Juric <mjuric@astro.washington.edu>
2018-07-08 14:29:52 -0700

    Adjustments to make it run on epyc

--- bin/jh-run.sh
+++ bin/jh-run.sh
@@ -4,10 +4,10 @@
 #
 
 docker \
-	run -it -p 9876:9876 \
-	--env-file=env \
+	run --rm -it -p 9876:9876 \
+	--env-file=/epyc/projects/sssc/env \
 	-v /epyc/opt/anaconda:/epyc/opt/anaconda \
 	-v /usr/local/share/jupyter/kernels:/usr/local/share/jupyter/kernels \
 	-v /epyc/projects/sssc/home:/home \
-	-v /epyc/opt/projects/sssc/lsst:/epyc/opt/projects/sssc/lsst \
+	-v /epyc/projects/sssc/lsst:/epyc/projects/sssc/lsst \
 	jupyterhub-sssc
